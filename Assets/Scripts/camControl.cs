aace6d31ab388f1491b5875264284da945e0cb02
Yusuf Khan <ykhan10@outlook.com>
2026-08-17 15:33:35 +0100

    slight progress on cam

--- Assets/Scripts/camControl.cs
+++ Assets/Scripts/camControl.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class camControl : MonoBehaviour
 {
+    Camera cam;
+    GameObject player;
+    Ray ray;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cam = Camera.main;
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ray = new Ray(
+            cam.transorm.position,
+            (player.transform.position - camera.transform.position).normalized
+        );
     }
 }
