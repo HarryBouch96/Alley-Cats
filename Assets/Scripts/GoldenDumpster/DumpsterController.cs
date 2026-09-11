6af067f479c8e0deb4b4a1081a6e0a8f824667ab
HarryBouch96 <108231003+HarryBouch96@users.noreply.github.com>
2026-09-11 17:44:26 +0100

    Include relocated building assets

--- Assets/Scripts/GoldenDumpster/DumpsterController.cs
+++ Assets/Scripts/GoldenDumpster/DumpsterController.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DumpsterController : MonoBehaviour
@@ -6,10 +5,18 @@ public class DumpsterController : MonoBehaviour
     public delegate void LevelCompleteHandler();
     public event LevelCompleteHandler LevelComplete;
 
+    private ParticleSystem particles;
+
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "cat")
         {
+            particles.Play();
             LevelComplete?.Invoke();
         }
     }
