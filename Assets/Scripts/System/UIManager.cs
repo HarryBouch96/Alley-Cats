84efba754d027c8510ebefb7949bb1321b0c135c
Yusuf Khan <ykhan10@outlook.com>
2026-09-14 01:45:25 +0100

    pause functionality

--- Assets/Scripts/System/UIManager.cs
+++ Assets/Scripts/System/UIManager.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -36,6 +37,11 @@ public class UIManager : MonoBehaviour
 
     [SerializeField]
     private GameObject noFishIconPrefab;
+
+    [SerializeField]
+    private GameObject pauseButtonPrefab;
+
+    private GameObject pauseButton;
 
     private Transform livesContainer;
     private Transform scoreContainer;
@@ -74,10 +80,24 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    public void pauseGame()
+    {
+        if (gameManager == null)
+            return;
+        gameManager.TogglePause();
+    }
+
     private void Start()
     {
         livesContainer = hud.transform.Find("Lives");
         scoreContainer = lvlCompleteScreen.transform.Find("Score");
+        pauseButton = Instantiate(pauseButtonPrefab, hud.transform, false);
+        Button btnComponent = pauseButton.GetComponent<Button>();
+        if (btnComponent != null)
+        {
+            // This tells the button: "When clicked, run the TogglePauseGame function"
+            btnComponent.onClick.AddListener(pauseGame);
+        }
 
         // Subscribe to GameManager state changes
         gameManager.StateChanged += StateChangedHandler;
